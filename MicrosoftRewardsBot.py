525b0c4ab00e5fb85f9313c9b44feb1a8d219f65
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2022-06-30 19:58:51 +0100

    Update MicrosoftRewardsBot.py

--- MicrosoftRewardsBot.py
+++ MicrosoftRewardsBot.py
@@ -28,7 +28,7 @@ BASE_URL = ""
 def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-a", "--accounts", nargs="*")
-    parser.add_argument("-h", "--headless", action="store_true")
+    parser.add_argument("-hl", "--headless", action="store_true")
     return parser.parse_args()
 
 # Define browser setup function
