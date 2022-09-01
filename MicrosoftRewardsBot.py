fd5aa55bac323320dd29101f8cc58a7bca5dd160
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2022-09-01 17:18:36 +0100

    Update MicrosoftRewardsBot.py

--- MicrosoftRewardsBot.py
+++ MicrosoftRewardsBot.py
@@ -16,6 +16,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
@@ -65,7 +66,7 @@ def browserSetup(isMobile: bool, user_agent: str = PC_USER_AGENT) -> WebDriver:
     if platform.system() == 'Linux':
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-    chrome_browser_obj = webdriver.Chrome(service=ChromeDriverManager().install(), options=options)
+    chrome_browser_obj = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return chrome_browser_obj
 
 # Define login function
