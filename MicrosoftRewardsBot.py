5832cb9c32e99e21fe1078606931f29a7dfd00ed
Mehdi Rtal <70097433+MehdiRtal@users.noreply.github.com>
2022-10-28 07:33:45 +0000

    Update MicrosoftRewardsBot.py

--- MicrosoftRewardsBot.py
+++ MicrosoftRewardsBot.py
@@ -1143,8 +1143,9 @@ def checkInternetConnection():
                 subprocess.check_output(["ping", "-c", "1", "8.8.8.8"], timeout=5)
             return
         except (subprocess.CalledProcessError, subprocess.TimeoutExpired):
-            prRed("[ERROR] No internet connection.")
-            time.sleep(1)
+            # prRed("[ERROR] No internet connection.")
+            # time.sleep(1)
+	    break
 
 def prRed(prt):
     print(f"\033[91m{prt}\033[00m")
